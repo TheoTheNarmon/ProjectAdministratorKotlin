2ed6e39438de4759fb51c13af9cd456c0f8a7e9e
Theo <narmontastheo@gmail.com>
2024-03-19 18:51:01 -0300

    fixed errors and added taxes

--- src/main/kotlin/Code.kt
+++ src/main/kotlin/Code.kt
@@ -1,6 +1,9 @@
 class Project(val tareas: MutableSet<Task> = mutableSetOf()) {
 
 }
+class Tax(val tax: Double){
+    fun tax() = this.tax
+}
 
 
 interface exercise{
@@ -27,17 +30,38 @@ interface exercise{
     fun percentage(x:Double,y:Double): Double{
         return (x*y)/100
     }
+
+    fun delay(): Int{
+        if(complexity == 1){
+            return 5
+        }
+        else if (complexity == 2){
+            return percentage(time,10.0).toInt()
+        }
+        else{
+            return percentage(time,20.0).toInt() + 8
+        }
+    }
 }
 
-class Task(val subtasks: MutableSet<Subtask> = mutableSetOf()): exercise{
+class Task(
+    val subtasks: MutableSet<Subtask> = mutableSetOf(),
+    override val time: Double,
+    override val complexity: Int,
+    val taxes: MutableSet<Tax> = mutableSetOf()
+    ): exercise{
     var filled: Double = 0.0
     fun filled() = this.filled
 
     override fun price(): Double{
         if (subtasks.size >= 3){
-            return super.price() + percentage(super.price(),4.0)
+            return super.price() + percentage(super.price(),4.0) + percentage(super.price(),taxes())
         }
-        else return super.price()
+        else return super.price() + percentage(super.price(),taxes())
+    }
+
+    fun taxes(): Double {
+        return taxes.sumOf {tax -> tax.tax()}
     }
     fun all(): Int{
         var i = 0
@@ -62,7 +86,7 @@ class Task(val subtasks: MutableSet<Subtask> = mutableSetOf()): exercise{
     }
 }
 
-class Subtask() : exercise{
+class Subtask(override val time: Double, override val complexity: Int) : exercise{
     var filled = false
     fun filled() = this.filled
 
